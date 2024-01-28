c223b0a846261f93c897ebdcd427998ebd1ab336
mifmif1 <michael.to.business@gmail.com>
2024-01-28 11:48:03 +0200

    some installs

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import Strings from './consts/Strings';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>{Strings.homeTitle}</Text>
+        </View>
+    )
+};
+
+export default Home;
+
